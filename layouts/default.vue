b176cc31dd135dfdee46d2b885865479f80ec917
= <=>
2022-06-07 15:42:01 -0300

    tailwind add

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="bg-gray-100 min-h-screen text-gray-900">
+        <nav class="bg-white shadow text-lg px-6 py-6">
+            <div class="container mx-auto flex item-center justify-between px-6">
+                <div>
+                    <NuxtLink to="/">Logo</NuxtLink>
+                </div>
+                <div>
+                    <ul class="flex space-x-12">
+                        <li>
+                            <NuxtLink to="/">Home</NuxtLink> 
+                        </li>
+                        <li>
+                            <NuxtLink to="/about">About</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/contact">Contact</NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+    const title = useState('title', () => "Blog Nuxt");
+</script>
+
+<style>
+    .router-link-active {
+        font-weight: bold;
+    }
+</style>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mx-auto w-1/2 py-8">
+        <h3 class="text-3xl font-bold">
+            <NuxtLink :to="`/posts/1`">Title of post</NuxtLink>
+        </h3>
+        <div class="post-meta text-gray-700 flex items-center space-x-2">
+            <div>07/06/2022</div>
+            <div>&middot;</div>
+            <div>Bruna</div>
+        </div>
+        <div class="post-content mt-4">
+            {{ $route.params.id }} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+    </div>
+</template> 
